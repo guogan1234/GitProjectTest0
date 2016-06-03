d3aa28c1c7654c7cc9fa363b51a839f5deb25ef8
zl <zhaolu@tjgeo.cn>
2016-06-03 16:35:58 +0800

    Signed-off-by: zl <zhaolu@tjgeo.cn>

--- DelImg.bat
+++ DelImg.bat
@@ -1,6 +1,13 @@
-del f:\TongYanSuiDao\Image\0\*.jpg
-del f:\TongYanSuiDao\Image\1\*.jpg
-del f:\TongYanSuiDao\Image\2\*.jpg
-del f:\TongYanSuiDao\Image\3\*.jpg
-del f:\TongYanSuiDao\Image\4\*.jpg
-del f:\TongYanSuiDao\Image\5\*.jpg
+del F:\TongYanSuiDao\Image\0\*.jpg
+del F:\TongYanSuiDao\Image\1\*.jpg
+del F:\TongYanSuiDao\Image\2\*.jpg
+del F:\TongYanSuiDao\Image\3\*.jpg
+del F:\TongYanSuiDao\Image\4\*.jpg
+del F:\TongYanSuiDao\Image\5\*.jpg
+
+del d:\TongYanSuiDao\Image\0\*.jpg
+del d:\TongYanSuiDao\Image\1\*.jpg
+del d:\TongYanSuiDao\Image\2\*.jpg
+del d:\TongYanSuiDao\Image\3\*.jpg
+del d:\TongYanSuiDao\Image\4\*.jpg
+del d:\TongYanSuiDao\Image\5\*.jpg
--- startGpio.bat
+++ startGpio.bat
@@ -1,4 +1,3 @@
 gpiotool.exe -b 0 -s 0:se,pu,ni
 gpiotool.exe -b 1 -s 0:se,pu,ni
 gpiotool.exe -b 2 -s 0:se,pu,ni
-pause
