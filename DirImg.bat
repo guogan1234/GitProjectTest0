6bfb0798a16772dafafe4706d56a1916e52346ca
zl <zhaolu@tjgeo.cn>
2016-06-12 14:38:27 +0800

    Signed-off-by: zl <zhaolu@tjgeo.cn>

---
+++ DirImg.bat
@@ -0,0 +1,14 @@
+dir /B /A-D f:\TongYanSuiDao\Image\0\*.jpg | find /V /C ""
+dir /B /A-D f:\TongYanSuiDao\Image\1\*.jpg | find /V /C ""
+dir /B /A-D f:\TongYanSuiDao\Image\2\*.jpg | find /V /C ""
+dir /B /A-D f:\TongYanSuiDao\Image\3\*.jpg | find /V /C ""
+dir /B /A-D f:\TongYanSuiDao\Image\4\*.jpg | find /V /C ""
+dir /B /A-D f:\TongYanSuiDao\Image\5\*.jpg | find /V /C ""
+
+dir /B /A-D d:\TongYanSuiDao\Image\0\*.jpg | find /V /C ""
+dir /B /A-D d:\TongYanSuiDao\Image\1\*.jpg | find /V /C ""
+dir /B /A-D d:\TongYanSuiDao\Image\2\*.jpg | find /V /C ""
+dir /B /A-D d:\TongYanSuiDao\Image\3\*.jpg | find /V /C ""
+dir /B /A-D d:\TongYanSuiDao\Image\4\*.jpg | find /V /C ""
+dir /B /A-D d:\TongYanSuiDao\Image\5\*.jpg | find /V /C ""
+pause
